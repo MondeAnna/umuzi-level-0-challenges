745e425f80f4278fe0ff9eae35ddc3077bcd70a3
MondeAnna <mondeanna@gmail.com>
2021-10-13 19:36:35 +0200

    Task 0.9: ordered unique vowels

---
+++ task_09.py
@@ -0,0 +1,16 @@
+
+def get_ordered_list_of_unique_vowels(string):
+    unique_vowels = {
+        vowel.lower():None
+        for vowel in string
+        if vowel.lower() in 'aeiou'
+    }
+
+    return unique_vowels.keys()
+
+
+def vowel_printer(string):
+    vowels = get_ordered_list_of_unique_vowels(string)
+    print(f'Vowels: {", ".join(vowels)}')
+
+
