413d70cdebd323694305bb5b7624f5da0103f6ca
MondeAnna <mondeanna@gmail.com>
2021-10-13 18:13:57 +0200

    Task 0.5: calculate the area of a triangle

---
+++ task_05.py
@@ -0,0 +1,9 @@
+
+def area_of_a_triangle(a, b, c):
+    """ area of a triangle calculated
+        using Heron's formula
+    """
+    s = (a + b + c) * 0.5
+    radicand = s * (s - a) * (s - b) * (s - c)
+    return radicand ** (1 / 2)
+
