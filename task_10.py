d13bd29762a86f99207212e0b15cd9ab56635814
MondeAnna <mondeanna@gmail.com>
2021-10-13 19:49:46 +0200

    Task 0.10: print common letters

---
+++ task_10.py
@@ -0,0 +1,10 @@
+
+def print_common_chars(string_one, string_two):
+    set_one = set(string_one)
+    set_two = set(string_two)
+
+    intersection = set_one.intersection(set_two)
+    common_letters = ', '.join(intersection)
+
+    print(f'Common letters: {common_letters}')
+
