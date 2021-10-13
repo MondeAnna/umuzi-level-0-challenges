ffe1caff8ed20e6f7552aae4c01bec96bf76718f
MondeAnna <mondeanna@gmail.com>
2021-10-13 19:09:59 +0200

    Task 0.8: time formatter

---
+++ task_08.py
@@ -0,0 +1,12 @@
+
+def time_formatter(passed_minutes):
+    minutes_in_an_hour = 60
+
+    hours = passed_minutes // minutes_in_an_hour
+    minutes = passed_minutes % minutes_in_an_hour
+
+    hours_descriptor = 'hour' if hours == 1 else 'hours'
+    minutes_descriptor = 'minute' if minutes == 1 else 'minutes'
+
+    return f'{hours} {hours_descriptor}, {minutes} {minutes_descriptor}'
+
